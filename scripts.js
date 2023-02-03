ad9b6b4cd228d9d326f63a8f79f501d4c410756b
Jack Crosbie <jackwcrosbie@gmail.com>
2023-02-03 21:20:39 +0000

    add singleRound function

--- scripts.js
+++ scripts.js
@@ -1,15 +1,37 @@
-const choices = ['Rock', 'Paper', 'Scissors']
+const choices = ['ROCK', 'PAPER', 'SCISSORS']
 
-function computerPlay(choices) {
-    return choices[Math.floor(Math.random()*choices.length)];
+function computerPlay() {
+    var computerChoice = [Math.floor(Math.random() * choices.length)];
+    return choices[computerChoice];
 }
 
-console.log(computerPlay(choices))
 
-
-function singleRound(playerSelection, computerSelection) {
-    let playerSelection = "";
-    let computerSelection = choices[Math.floor(Math.random()*choices.length)];
+function singleRound(computerSelection, playerSelection){
+    console.log("Your choice: " + playerSelection + "\nComputer's choice: " + computerSelection);
+    //In event of a tie
+    if(computerSelection == playerSelection){
+        console.log("You Tied");
+    }
+    //Winning selections
+    if(computerSelection == "ROCK"){
+        if(playerSelection == "PAPER"){
+            console.log("Congratulations! you've won");           
+        } else if(playerSelection == "SCISSOR"){
+            console.log("You lose, Rock beats Scissors!");
+        }
+    } else if(computerSelection == "PAPER"){
+        if(playerSelection == "ROCK"){
+            console.log("You lose, Paper beats Rock!");       
+        } else if(playerSelection == "SCISSOR"){
+            console.log("Congratulations! you've won");
+        }
+    } else if(computerSelection == "SCISSOR"){
+        if(playerSelection == "ROCK"){
+            console.log("Congratulations! you've won");           
+        } else if(playerSelection == "PAPER"){
+            console.log("You lose, Scissor beats Paper!");
+        }
+    }
 }
 
 const playerSelection = "rock";

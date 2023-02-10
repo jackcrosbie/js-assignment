cae07e1f86c9638833d64d3aced80d0d53a5d524
Jack Crosbie <jackwcrosbie@gmail.com>
2023-02-10 16:01:04 +0000

    updated Js

--- scripts.js
+++ scripts.js
@@ -6,70 +6,84 @@ const choiceBtns = document.querySelectorAll('.choiceBtn');
 let playerScore = 0;
 let computerScore = 0;
 
+
 choiceBtns.forEach(button => button.addEventListener("click", () => {
-    let player = button.textContent.toUpperCase();
-    let computer = computerPlay();
-    playerText.textContent = `Player: ${player}`;
-    computerText.textContent = `Computer: ${computer}`;
-    let result = singleRound(computer, player);
-    resultText.textContent = `Result: ${result}`;
-    updateScores(result);
+  let player = button.textContent.toUpperCase();
+  let computer = computerPlay();
+  playerText.textContent = `Player: ${player}`;
+  computerText.textContent = `Computer: ${computer}`;
+  let result = singleRound(computer, player);
+  resultText.textContent = `Result: ${result}`;
+  updateScores(result);
 }));  
-   
+
+
 function computerPlay() {
     var computerChoice = Math.floor(Math.random() * choices.length);
     return choices[computerChoice];
 }
 
+
 function singleRound(computerSelection, playerSelection) {
-    playerSelection = playerSelection.toUpperCase();
-    let result;
-    
-    if (computerSelection === playerSelection) {
-      result = "It's a draw!";
-    } else if (computerSelection === "ROCK") {
-      if (playerSelection === "PAPER") {
-        result = "You win!";
-      } else {
-        result = "You lose!";
-      }
+  playerSelection = playerSelection.toUpperCase();
+  computerSelection = computerSelection.toUpperCase();
+  let result;
+
+  if (playerSelection === "ROCK") {
+    if (computerSelection === "SCISSORS") {
+      result = "You win!";
     } else if (computerSelection === "PAPER") {
-      if (playerSelection === "ROCK") {
-        result = "You lose!";
-      } else {
-        result = "You win!";
-      }
+      result = "You lose!";
+    } else {
+      result = "It's a draw!";
+    }
+  } else if (playerSelection === "PAPER") {
+    if (computerSelection === "ROCK") {
+      result = "You win!";
     } else if (computerSelection === "SCISSORS") {
-      if (playerSelection === "ROCK") {
-        result = "You win!";
-      } else {
-        result = "You lose!";
-      }
+      result = "You lose!";
+    } else {
+      result = "It's a draw!";
     }
-    return result;
-}
-
-function updateScores(result) {
-    if (result === "You win!") {
-      playerScore++;
-    } else if (result === "You lose!") {
-      computerScore++;
-    }
-  
-    document.getElementById("playerScore").innerHTML = `Player Score: ${playerScore}`;
-    document.getElementById("computerScore").innerHTML = `Computer Score: ${computerScore}`;
-    console.log("Player Score: " + playerScore + " Computer Score: " + computerScore);
-    checkWinner();
-}
-
-function checkWinner() {
-    if (playerScore === 5) {
-      resultText.textContent = "Player wins the game!";
-      playerScore = 0;
-      computerScore = 0;
-    } else if (computerScore === 5) {
-      resultText.textContent = "Computer wins the game!";
-      playerScore = 0;
-      computerScore = 0;
+  } else if (playerSelection === "SCISSORS") {
+    if (computerSelection === "PAPER") {
+      result = "You win!";
+    } else if (computerSelection === "ROCK") {
+      result = "You lose!";
+    } else {
+      result = "It's a draw!";
     }
   }
+
+  return result;
+}
+
+
+function updateScores(result) {
+  if (result === "You win!") {
+    playerScore++;
+  } else if (result === "You lose!") {
+    computerScore++;
+  }
+
+  document.getElementById("playerScore").innerHTML = `Player Score: ${playerScore}`;
+  document.getElementById("computerScore").innerHTML = `Computer Score: ${computerScore}`;
+
+  checkWinner();
+}
+
+
+function checkWinner() {
+  if (playerScore === 5) {
+    resultText.textContent = "Player wins the game!";
+    playerScore = 0;
+    computerScore = 0;
+  } else if (computerScore === 5) {
+    resultText.textContent = "Computer wins the game!";
+    playerScore = 0;
+    computerScore = 0;
+  }
+
+  document.getElementById("playerScore").innerHTML = `Player Score: ${playerScore}`;
+  document.getElementById("computerScore").innerHTML = `Computer Score: ${computerScore}`;
+}
